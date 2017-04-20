1ce9fdd3e8a181438330d824b7b6673c5d732e90
Cristina Rodriguez <yosola@gmail.com>
2017-04-20 16:48:06 -0700

    first 200 results perfectly working in a json

--- searchapi.py
+++ searchapi.py
@@ -1,8 +1,9 @@
 from nytimesarticle import articleAPI
 from secret import API
 import pprint
-import csv
-
+import json
+import os
+import time
 
 def parse_articles(articles):
     '''
@@ -10,6 +11,7 @@ def parse_articles(articles):
     the articles into a list of dictionaries
     '''
     news = []
+    print articles.keys()
     for i in articles['response']['docs']:
         dic = {}
         dic['id'] = i['_id']
@@ -17,7 +19,7 @@ def parse_articles(articles):
             dic['abstract'] = i['abstract'].encode("utf8")
         dic['headline'] = i['headline']['main'].encode("utf8")
         dic['desk'] = i['news_desk']
-        dic['date'] = i['pub_date'][0:10] # cutting time of day.
+        dic['date'] = i['pub_date'] 
         dic['section'] = i['section_name']
         if i['snippet'] is not None:
             dic['snippet'] = i['snippet'].encode("utf8")
@@ -27,50 +29,105 @@ def parse_articles(articles):
         dic['word_count'] = i['word_count']
         # locations
         locations = []
-        for x in range(0,len(i['keywords'])):
+        for x in range(0, len(i['keywords'])):
             if 'glocations' in i['keywords'][x]['name']:
                 locations.append(i['keywords'][x]['value'])
         dic['locations'] = locations
         # subject
         subjects = []
-        for x in range(0,len(i['keywords'])):
+        for x in range(0, len(i['keywords'])):
             if 'subject' in i['keywords'][x]['name']:
                 subjects.append(i['keywords'][x]['value'])
-        dic['subjects'] = subjects   
+        dic['subjects'] = subjects
         news.append(dic)
     return(news)
 
-def get_articles():
-   
+
+def get_articles(begin_date=None, end_date=None):
+
     api = articleAPI(API)
     all_articles = []
-    pages = [0, 1, 2]
-    for page in pages:
-        request = api.search(q = 'Mexico',
-               fq = {'source':['Reuters','AP', 'The New York Times']},
-               begin_date = '20160101', 
-               page=str(page))
+    
+    if begin_date is None:
+        begin_date = '20150101'
+
+    if end_date is None:
+        end_date = '20170420'
+
+
+    for page in range(0, 20):
+        request = api.search(q='Mexico',
+               fq={'glocations.contains': 'Mexico'},
+               begin_date=begin_date,
+               end_date=end_date,
+               sort='newest',
+               page=page)
         articles = parse_articles(request)
         all_articles = all_articles + articles
-    return(all_articles)
+        time.sleep(15)
+    return all_articles
 
 
-mexico_search =  get_articles()
-print "********************* Your response begin here"
-print len(mexico_search)
-pprint.pprint(mexico_search)
-
-keys = mexico_search[0].keys()
-with open('mex-mentions.csv', 'wb') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(mexico_search)
+# mexico_search = get_articles()
+# print "********************* Your response begin here"
+# print type(mexico_search)
+# print len(mexico_search)
+# pprint.pprint(mexico_search)
+# with open('data.json', 'ab') as outfile:
+#     json.dump(mexico_search, outfile, sort_keys=True, indent=4,
+#               ensure_ascii=False)
 
 
+if __name__ == "__main__":
 
+    prompt = """ Hello. This is your scrappy program. Do you want:
+    1. to know the date of the last article
+    2. See the whole json
+    3. Make a search
+    4. Quit
+    """
+    while True:
+        ask_user = int(raw_input(prompt))
 
+        if ask_user == 1:
+            if not os.path.isfile('/Users/cristina/src/NewYorkTimes/data.json'):
+                print "There is not file yet"
+            else:
+                with open('data.json') as feedsjson:
+                    feeds = json.load(feedsjson)
+                    last_article = feeds[-1]['date'][0:10]
+                    end_date = last_article.replace("-", "")
+                    print end_date
+ 
+        elif ask_user == 2:
+            if not os.path.isfile('/Users/cristina/src/NewYorkTimes/data.json'):
+                print "There is not file yet"
+            else:
+                with open('data.json') as feedsjson:
+                    feeds = json.load(feedsjson)
+                    pprint.pprint(feeds)
 
+        elif ask_user == 3:
+            if not os.path.isfile('/Users/cristina/src/NewYorkTimes/data.json'):
+                print "the begin_date is 20150101"
+                search = get_articles()
+                with open('data.json', 'wr') as outfile:
+                    json.dump(search, outfile, sort_keys=True, indent=4, ensure_ascii=False)
 
+            else:
+                with open('data.json', 'ab') as feedsjson:
+                    feeds = json.load(feedsjson)
+                    last_article = feeds[-1]['date'][0:10]
+                    end_date = last_article.replace("-", "")
+                    print "the end_date is %s" % (end_date)
+                    search = get_articles(end_date)
+                    feeds.append(search)
+                    with open('data.json', 'ab') as outfile:
+                        json.dumps(feeds, outfile, sort_keys=True, indent=4, ensure_ascii=False)
 
+        elif ask_user == 4:
+            break
 
+        else:
+            print "I don't understand that directive"
 
