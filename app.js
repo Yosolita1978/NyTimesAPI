8dc3b20116313e38ac7b9254bb3c9097d63107b7
cristina <yosola@gmail.com>
2018-01-24 16:22:25 -0800

    first attempt using d3

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-var quotes = [
-  "Go ahead, make my day.",
-  "I'll be back.",
-  "May the Force be with you.",
-  "There's no place like home.",
-  "You're gonna need a bigger boat."
-];
+(function(){
+    var width = 500;
+    var height = 500;
+
+    var svg = d3.select("#chart")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width)
+    .append("g")
+    .attr("transform", "translate(100,400)")
+
+    var simulation = d3.forceSimulation()
+    .force("x", d3.forceX(width / 2).strength(0.05))
+    .force("y", d3.forceY(height / 2).strength(0.05))
+
+    d3.queue()
+    .defer(d3.csv, "output_trump.csv")
+    .await(ready)
+
+    function ready(error, datapoints){
+
+        var circles = svg.selectAll(".word")
+        .data(datapoints)
+        .enter().append("circles")
+        .attr("class", "word")
+        .attr("r", 100)
+        .attr("fill", "red")
+        
+        simulation.nodes(datapoints)
+        .on('tick', ticked)
+
+        function ticked(){
+            circles
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y)
+        }
+    }
+
+})();
